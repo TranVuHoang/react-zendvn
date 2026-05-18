c23522746d087a8b8db910fe0be74192a7ba75cb
TranVuHoang <tranvuhoangjr@gmail.com>
2026-05-19 06:55:18 +0700

    #11. tách commponent Lesson

--- ch01/src/components/Course.jsx
+++ ch01/src/components/Course.jsx
@@ -1,19 +1,24 @@
+import Lesson from "./Lesson";
+
 const Course = () => {
   return (
-    <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-      <div className="panel panel-info">
-        <div className="panel-heading">
-          <h3 className="panel-title">ReactJS</h3>
+    <>
+      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        <div className="panel panel-info">
+          <div className="panel-heading">
+            <h3 className="panel-title">ReactJS</h3>
+          </div>
+          <div className="panel-body">
+            <ul className="list-group">
+              <Lesson />
+              <Lesson />
+              <Lesson />
+            </ul>
+          </div>
         </div>
-        <div className="panel-body">
-          <ul className="list-group">
-            <li className="list-group-item">Item 1</li>
-            <li className="list-group-item">Item 2</li>
-            <li className="list-group-item">Item 3</li>
-          </ul>
-        </div>
+        <button className="btn btn-success mb-2">Register</button>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ ch01/src/components/Lesson.jsx
@@ -0,0 +1,5 @@
+const Lesson = () => {
+  return <li className="list-group-item">123</li>;
+};
+
+export default Lesson;
